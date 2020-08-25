8fcc07b2626c35d579dc2f1e74aa19b8443d0c34
nikhilmahajan918 <nikhilmahajan918@gmail.com>
2020-08-25 20:25:53 +0530

    initial commit

--- Todobucket/wsgi.py
+++ Todobucket/wsgi.py
@@ -10,6 +10,15 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Todobucket.settings")
+
+application = Cling(get_wsgi_application())
+
+if os.getcwd() == '/app':
+    from whitenoise.django import DjangoWhiteNoise
+    application = DjangoWhiteNoise(application)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Todobucket.settings')
 
