8952c9be80bb5371b0eb5e81393477e3a6718a22
nikhilmahajan918 <nikhilmahajan918@gmail.com>
2020-08-25 19:59:31 +0530

    initial commit

--- Todobucket/settings.py
+++ Todobucket/settings.py
@@ -115,6 +115,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
